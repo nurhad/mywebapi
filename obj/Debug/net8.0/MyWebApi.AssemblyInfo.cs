c699dbeaa6c0dcae687cc11b4dafa28a5fe21f55
m.barokah <m.barokah@first-resources.com>
2025-11-27 15:09:21 +0700

    initial commit

--- obj/Debug/net8.0/MyWebApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyWebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8589f7f8ffa66c14c76d0d2fc4429b630650298")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
