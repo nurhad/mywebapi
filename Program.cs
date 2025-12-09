77512acf9bb02f5caf14d589202562a65603990c
m.barokah <m.barokah@first-resources.com>
2025-12-09 09:03:35 +0700

    test

--- Program.cs
+++ Program.cs
@@ -18,7 +18,8 @@ app.UseHttpsRedirection();
 
 var summaries = new[]
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    // "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    "Membeku", "Menyegarkan", "Dingin", "Sejuk", "Hangat sedang", "Hangat", "Teduh", "Panas", "Terik", "Membakar"
 };
 
 app.MapGet("/weatherforecast", () =>
